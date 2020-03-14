85f8a3faed35115f06c5736c603c5ff10145c0b6
Nhan Nguyen <khanhnhan5391@gmail.com>
2020-03-14 01:22:24 -0700

    Adding some TODO notes to the skeleton

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
+
+// TODO: Make this persisting to a file instead of memory.
 let db = new sqlite3.Database(':memory:');
 db.exec(`
 CREATE TABLE IF NOT EXISTS FrequencyReports (
@@ -16,6 +18,9 @@ INSERT INTO FrequencyReports (Word, Frequency) VALUES ('新冠病毒', 100);
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // Selecting a random top word from FrequencyReports and display.
+  // TODO: Improve the query, make it a bit more sophisticated, for
+  // example, giving the words from the last day more weight than
+  // the days prior.
   db.all(`SELECT Word, SUM(Frequency) AS Frequency
           FROM FrequencyReports
           GROUP BY Word
