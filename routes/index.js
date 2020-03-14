5822b586a9b90539c936535e5b4838b6e00017ce
Nhan Nguyen <khanhnhan5391@gmail.com>
2020-03-14 01:18:20 -0700

    Adding sqlite3 and a naive query for the dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,36 @@
 var express = require('express');
 var router = express.Router();
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database(':memory:');
+db.exec(`
+CREATE TABLE IF NOT EXISTS FrequencyReports (
+  Word TEXT NOT NULL,
+  Frequency INTEGER,
+  Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO FrequencyReports (Word, Frequency) VALUES ('出租车', 100);
+INSERT INTO FrequencyReports (Word, Frequency) VALUES ('新冠病毒', 100);
+`)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // Selecting a random top word from FrequencyReports and display.
+  db.all(`SELECT Word, SUM(Frequency) AS Frequency
+          FROM FrequencyReports
+          GROUP BY Word
+          ORDER BY Frequency DESC
+          LIMIT 10`, [], (err, topWords) => {
+      if (err) {
+        throw err;
+      }
+      const randomIndex = Math.floor(Math.random() * topWords.length);
+      res.render('index', { word: topWords[randomIndex].Word });
+    });
 });
 
 router.post('/freqreport', function(req, res) {
+  // TODO: Insert to FrequencyReports table here.
   res.send('POST request: ' + JSON.stringify(req.body));
 });
 
